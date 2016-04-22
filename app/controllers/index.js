3caee8678bd36e5f690676a40d2362062be309b1
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-04-21 23:08:22 -0300

    controller index promise

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,34 +7,34 @@ const async = require('async');
 const Model = require('../models/v1/participant');
 
 let _findByYear = (year, cb) => {
-    Model.find({ year: year }, cb);
+    return Model.find({ year: year });
 };
 
 let _findByYearAndDay = (year, day, cb) => {
-    Model.find({ year: year, 'days.name': day }, cb);
+    return Model.find({ year: year, 'days.name': day });
 };
 
 let _getYears = cb => {
-    Model.distinct('year', (err, years) => {
-        if (err) return cb(err);
-        return cb(null, years.sort());
-    });
+    return Model.distinct('year');
 };
 
 let _yearlyRegistration = (years, result, callback) => {
     let values = [];
     async.each(years, (year, cb) => {
-        _findByYear(year, (err, participants) => {
-            if (err) return cb(err);
-            let count = 0;
-            async.each(participants, (participant, cbParticipant) => {
-                count += participant.days.length;
-                cbParticipant();
-            }, () => {
-                values.push({ label: year, value: count });
-                cb();
-            });
-        });
+        _findByYear(year)
+            .then(participants => {
+                let count = 0;
+                async.each(participants, (participant, cbParticipant) => {
+                    count += participant.days.length;
+                    cbParticipant();
+                }, () => {
+                    values.push({ label: year, value: count });
+                    cb();
+                });
+            })
+            .catch(err => {
+                cb(err);
+            })
     }, err => {
         if (err) return callback(err);
         result.push({ key: 'Registration', values: values.sort((a, b) => a.label > b.label) })
@@ -45,17 +45,20 @@ let _yearlyRegistration = (years, result, callback) => {
 let _yearlyRegistrationPresent = (years, result, callback) => {
     let values = [];
     async.each(years, (year, cb) => {
-        _findByYear(year, (err, participants) => {
-            if (err) return cb(err);
-            let count = 0;
-            async.each(participants, (participant, cbParticipant) => {
-                count += participant.days.filter(day => day.present === true).length;
-                cbParticipant();
-            }, () => {
-                values.push({ label: year, value: count });
-                cb();
-            });
-        });
+        _findByYear(year)
+            .then(participants => {
+                let count = 0;
+                async.each(participants, (participant, cbParticipant) => {
+                    count += participant.days.filter(day => day.present === true).length;
+                    cbParticipant();
+                }, () => {
+                    values.push({ label: year, value: count });
+                    cb();
+                });
+            })
+            .catch(err => {
+                cb(err);
+            })
     }, err => {
         if (err) return callback(err);
         result.push({ key: 'Present', values: values.sort((a, b) => a.label > b.label) })
@@ -66,17 +69,20 @@ let _yearlyRegistrationPresent = (years, result, callback) => {
 let _yearlyRegistrationAbsent = (years, result, callback) => {
     let values = [];
     async.each(years, (year, cb) => {
-        _findByYear(year, (err, participants) => {
-            if (err) return cb(err);
-            let count = 0;
-            async.each(participants, (participant, cbParticipant) => {
-                count += participant.days.filter(day => { return day.present === false }).length;
-                cbParticipant();
-            }, () => {
-                values.push({ label: year, value: count });
-                cb();
-            });
-        });
+        _findByYear(year)
+            .then(participants => {
+                let count = 0;
+                async.each(participants, (participant, cbParticipant) => {
+                    count += participant.days.filter(day => { return day.present === false }).length;
+                    cbParticipant();
+                }, () => {
+                    values.push({ label: year, value: count });
+                    cb();
+                });
+            })
+            .catch(err => {
+                cb(err);
+            })
     }, err => {
         if (err) return callback(err);
         result.push({ key: 'Absent', values: values.sort((a, b) => a.label > b.label) })
@@ -87,22 +93,25 @@ let _yearlyRegistrationAbsent = (years, result, callback) => {
 let _getNumberParticipantsDaily = (year, present, callback) => {
     let values = [];
     async.each([1, 2, 3, 4, 5], (day, cbDay) => {
-        _findByYearAndDay(year, day, (err, participants) => {
-            if (err) return cbDay(err);
-            if (present === null) {
-                values.push({ label: day, value: participants.length });
-                return cbDay();
-            }
-            let value = 0;
-            async.each(participants, (participant, cbParticipant) => {
-                let exist = participant.days.filter(d => d.name === day && d.present === present)[0];
-                if (exist) value += 1;
-                cbParticipant();
-            }, () => {
-                values.push({ label: day, value: value });
-                cbDay();
-            });
-        });
+        _findByYearAndDay(year, day)
+            .then(participants => {
+                if (present === null) {
+                    values.push({ label: day, value: participants.length });
+                    return cbDay();
+                }
+                let value = 0;
+                async.each(participants, (participant, cbParticipant) => {
+                    let exist = participant.days.filter(d => d.name === day && d.present === present)[0];
+                    if (exist) value += 1;
+                    cbParticipant();
+                }, () => {
+                    values.push({ label: day, value: value });
+                    cbDay();
+                });
+            })
+            .catch(err => {
+                cbDay(err);
+            })
     }, err => {
         if (err) callback(err);
         return callback(null, values.sort((a, b) => a.label > b.label));
@@ -114,20 +123,23 @@ let _dailyParticipant = (present, callback) => {
         callback = present;
         present = null;
     }
-    _getYears((err, years) => {
-        if (err) return callback(err);
-        let result = [];
-        async.each(years, (year, cb) => {
-            _getNumberParticipantsDaily(year, present, (err, values) => {
-                if (err) cb(err);
-                result.push({ key: year, values: values });
-                return cb();
+    _getYears()
+        .then(years => {
+            let result = [];
+            async.each(years, (year, cb) => {
+                _getNumberParticipantsDaily(year, present, (err, values) => {
+                    if (err) cb(err);
+                    result.push({ key: year, values: values });
+                    return cb();
+                });
+            }, err => {
+                if (err) callback(err);
+                return callback(null, result.sort((a, b) => a.key > b.key));
             });
-        }, err => {
-            if (err) callback(err);
-            return callback(null, result.sort((a, b) => a.key > b.key));
+        })
+        .catch(err => {
+            return callback(err);
         });
-    });
 };
 
 module.exports = {
@@ -137,35 +149,42 @@ module.exports = {
     },
 
     yearlyParticipants : (req, res, next) => {
-        _getYears((err, years) => {
-            let values = [];
-            async.each(years, (year, cb) => {
-                let query = { year: year };
-                Model.count(query, (err, count) => {
-                    if (err) return cb(err);
-                    values.push({ label: year, value: count });
-                    cb();
+        _getYears()
+            .then(years => {
+                let values = [];
+                async.each(years, (year, cb) => {
+                    let query = { year: year };
+                    Model.count(query, (err, count) => {
+                        if (err) return cb(err);
+                        values.push({ label: year, value: count });
+                        cb();
+                    });
+                }, err => {
+                    if (err) return res.status(500).json(err);
+                    return res.status(200).json(values.sort((a, b) => a.label > b.label));
                 });
-            }, err => {
-                if (err) return res.status(500).json(err);
-                return res.status(200).json(values.sort((a, b) => a.label > b.label));
-            });
-        });
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
     },
 
     yearlyRegistrations : (req, res, next) => {
-        _getYears((err, years) => {
-            if (err) return res.status(500).json(err);
-            let result = [];
-            async.parallel({
-                registration : async.apply(_yearlyRegistration, years, result),
-                present : async.apply(_yearlyRegistrationPresent, years, result),
-                absent : async.apply(_yearlyRegistrationAbsent, years, result)
-            }, err => {
-                if (err) return res.status(500).json(err);
-                return res.status(200).json(result);
-            });
-        });
+        _getYears()
+            .then(years => {
+                let result = [];
+                async.parallel({
+                    registration : async.apply(_yearlyRegistration, years, result),
+                    present : async.apply(_yearlyRegistrationPresent, years, result),
+                    absent : async.apply(_yearlyRegistrationAbsent, years, result)
+                }, err => {
+                    if (err) return res.status(500).json(err);
+                    return res.status(200).json(result);
+                });
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
     },
 
     dailyParticipants : (req, res, next) => {
