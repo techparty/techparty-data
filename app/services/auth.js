7772162f59df9d63f85e79bc0a48713690c62788
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-11-15 22:18:14 -0200

    indent auth service

--- app/services/auth.js
+++ app/services/auth.js
@@ -1,15 +1,13 @@
-'use strict';
-
 const passport = require('passport');
 
 module.exports = {
 
-    isAuthenticated : (req, res, next) => {
-        if (req.isAuthenticated()) return next();
+  isAuthenticated : (req, res, next) => {
+    if (req.isAuthenticated()) return next();
 
-        passport.authenticate('local', {
-            failureRedirect: '/auth/signin',
-        })(req, res, next);
-    }
+    passport.authenticate('local', {
+      failureRedirect: '/auth/signin',
+    })(req, res, next);
+  },
 
 };
